adef7e730a94d28948b7a08464231b18ef7a5f24
Kamil Stepczuk <k.stepczuk@hotmail.com>
2019-04-24 10:27:27 +0200

    filter units

--- api/dataSource/rc.js
+++ api/dataSource/rc.js
@@ -5,12 +5,37 @@ const USER_ID = process.env.USER_ID;
 const TOKEN = process.env.TOKEN;
 const RC_PATH = process.env.RC_PATH;
 
+const UNITS = [
+  'years',
+  'y',
+  'quarters',
+  'Q',
+  'months',
+  'M',
+  'weeks',
+  'w',
+  'days',
+  'd',
+  'hours',
+  'h',
+  'minutes',
+  'm',
+  'seconds',
+  's',
+  'milliseconds',
+  'ms',
+]
+
 exports.getLatestEmoji = async since => {
   const amount = since.match(/\d+/g)[0];
   const unit =  since.match(/[a-zA-Z]+/g)[0];
 
   if (!amount || !unit) {
     throw new Error('WRONG!');
+  }
+
+  if (!UNITS.includes(unit)) {
+    return `Hello Sir, chicken tikka masala team cannot help you, please use one of these units ${UNITS.join(', ')} Sir.`
   }
 
   try {
